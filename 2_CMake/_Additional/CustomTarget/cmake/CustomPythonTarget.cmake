7d647917393d5fb01bc8f41ec4cc09e908ae0cc7
franneck94 <schaffranek.jan@web.de>
2024-07-13 13:38:21 +0200

    up

--- 2_CMake/_Additional/CustomTarget/cmake/CustomPythonTarget.cmake
+++ 2_CMake/_Additional/CustomTarget/cmake/CustomPythonTarget.cmake
@@ -2,9 +2,10 @@ set(CUSTOM_TARGET_NAME "CustomPythonTarget")
 if(NOT TARGET ${CUSTOM_TARGET_NAME})
     add_custom_target(
         ${CUSTOM_TARGET_NAME}
-        COMMAND python CustomPythonTarget.py --directory ${PROJECT_SOURCE_DIR}/src
+        COMMAND python CustomPythonTarget.py --input_dir ${PROJECT_SOURCE_DIR}/src --output_dir ${PROJECT_BINARY_DIR}
         WORKING_DIRECTORY
         ${PROJECT_SOURCE_DIR}/scripts
+        # DEPENDS Main
     )
     add_dependencies(${CUSTOM_TARGET_NAME} Main)
 endif()
--- 2_CMake/_Additional/CustomTarget/scripts/CustomPythonTarget.py
+++ 2_CMake/_Additional/CustomTarget/scripts/CustomPythonTarget.py
@@ -1,5 +1,6 @@
 import argparse
 import glob
+from pathlib import Path
 
 
 def num_lines(fname: str) -> int:
@@ -11,17 +12,29 @@ def main() -> None:
         description="CustomPythonTarget",
     )
 
-    directory = parser.add_argument(
-        "--directory",
+    parser.add_argument(
+        "--input_dir",
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        "--output_dir",
         type=str,
         required=True,
     )
 
     args = parser.parse_args()
-    directory = args.directory
+    input_dir: str = args.input_dir
+    output_dir: str = args.output_dir
 
-    for file in glob.glob(f"{directory}/*.cc"):
-        print(f"File: {file} has {num_lines(file)} lines")
+    content = []
+    for file in glob.glob(f"{input_dir}/*.cc"):
+        content.append(f"File: {file} has {num_lines(file)} lines")
+
+    output_file = Path(output_dir) / "Stats.txt"
+    with output_file.open("w") as f:
+        f.writelines(content)
 
 
 if __name__ == "__main__":
